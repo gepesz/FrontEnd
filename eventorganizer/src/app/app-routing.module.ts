df6e60b1d451dcb75aca4798b7751b9bd20d7c83
BHedi89 <hedi.besenyei89@gmail.com>
2020-02-04 13:56:22 +0100

    app-header display in a bowser

--- eventorganizer/src/app/app-routing.module.ts
+++ eventorganizer/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainSiteComponent } from './components/main-site/main-site.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: MainSiteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- eventorganizer/src/app/app.module.ts
+++ eventorganizer/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './components/event-list/event-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainSiteComponent } from './components/main-site/main-site.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventListComponent
+    EventListComponent,
+    HeaderComponent,
+    MainSiteComponent
   ],
   imports: [
     BrowserModule,
