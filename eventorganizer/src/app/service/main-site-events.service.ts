efa05423cc84ce52206333a48fc39f3f1c6164d0
BHedi89 <hedi.besenyei89@gmail.com>
2020-02-07 15:00:00 +0100

    man site service

--- eventorganizer/src/app/service/main-site-events.service.ts
+++ eventorganizer/src/app/service/main-site-events.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { EventResponse } from '../interfaces/event-response';
+import { Event } from '../interfaces/event';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainSiteEventsService {
 
-  private events: Observable<Event[]>;
-  private readonly SERVER_URL = "http://192.168.1.53:8080/events"
+  private events: BehaviorSubject<Event[]>;
+  private readonly SERVER_URL = "http://192.168.1.53:8080/home"
 
   constructor(private http: HttpClient) { 
-
+    this.events = new BehaviorSubject([])
   }
 
-  getEvents(): Observable<Event[]> {
+  getEvents(): BehaviorSubject<Event[]> {
+    this.http.get<EventResponse>(this.SERVER_URL, {withCredentials: true})
+      .subscribe(resp => {this.updateEvents(resp);});
+    return this.events;
+  }
 
-    
-    return null;
+  updateEvents(response: EventResponse) {
+    if(response.success){
+      this.events.next(response.events);
+    }
   }
 
 
