173ac2619bc7297d8feae83a758a8a71c07a31e6
gepesz <roland.surin@gmail.com>
2020-02-13 15:31:34 +0100

    modify events service

--- eventorganizer/src/app/service/event.service.ts
+++ eventorganizer/src/app/service/event.service.ts
@@ -3,11 +3,7 @@ import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { EventResponse } from '../interfaces/event-response';
 import { Event } from '../interfaces/event';
-<<<<<<< HEAD
-import { Constants } from '../interfaces/constants';
-=======
 import { environment } from 'src/environments/environment';
->>>>>>> 710e8f04cbece9ce1f3f676bc317d4d1c858e589
 
 
 @Injectable({
@@ -17,21 +13,13 @@ import { environment } from 'src/environments/environment';
 export class EventService {
 
   private events: BehaviorSubject<Event[]>;
-<<<<<<< HEAD
-  private readonly GETEVENTS_URL = Constants.hostName +  "/events";
-  private readonly GETFILTEREDEVENTS_URL = Constants.hostName +  "/eventsfilter";
-=======
   private readonly SERVER_URL = environment.serverUrl;
-  private readonly GETEVENTS_URL = environment.serverUrl + '/events';
-  private readonly GETFILTEREDEVENTS_URL = environment.serverUrl + '/eventsfilter';
->>>>>>> 710e8f04cbece9ce1f3f676bc317d4d1c858e589
-
   constructor(private http: HttpClient) {
     this.events = new BehaviorSubject([]);
   }
 
   getEvents(): BehaviorSubject<Event[]> {
-    this.http.get<EventResponse>(this.GETEVENTS_URL, { withCredentials: true })
+    this.http.get<EventResponse>(this.SERVER_URL + "/events", { withCredentials: true })
       .subscribe(resp => {
         this.updateEvent(resp);
       });
@@ -39,7 +27,7 @@ export class EventService {
   }
 
   getEventsByFilter(): BehaviorSubject<Event[]> {
-    this.http.post<EventResponse>(this.GETFILTEREDEVENTS_URL, { withCredentials: true })
+    this.http.post<EventResponse>(this.SERVER_URL + "/eventsfilter", { withCredentials: true })
       .subscribe(resp => {
         this.updateEvent(resp);
       });
@@ -52,12 +40,12 @@ export class EventService {
     }
   }
 
-  getMyEvents(id:number): BehaviorSubject<Event[]> {
-     this.http.get<EventResponse>(this.SERVER_URL + '/myevents/' + id, {withCredentials:true})
-        .subscribe(resp => {
-            this.updateEvent(resp);
-        });
-      return this.events;
+  getMyEvents(id: number): BehaviorSubject<Event[]> {
+    this.http.get<EventResponse>(this.SERVER_URL + '/myevents/' + id, { withCredentials: true })
+      .subscribe(resp => {
+        this.updateEvent(resp);
+      });
+    return this.events;
   }
 
 }
